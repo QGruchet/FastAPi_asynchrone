1634fbba5fdadb27fa08983493b8838b2c7de32e
Quentin Gruchet <55387771+QGruchet@users.noreply.github.com>
2024-01-10 13:48:44 +0000

    [features]: ajout de l'accès à la bd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import *
 from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -9,6 +10,15 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 Base = declarative_base()
+
+
+class Process(Base):
+    __tablename__ = "processes"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+
+Base.metadata.create_all(bind=engine)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
