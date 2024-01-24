cc85f4875cf661da3e47581a6d261548243b90c2
nlsferrara@gmail.com <100352279+nlsferrara@users.noreply.github.com>
2024-01-24 17:14:34 +0100

    create MODPROC_TP1

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import uvicorn
 
 app = FastAPI()
-
+"""
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
@@ -22,7 +23,11 @@ Base.metadata.create_all(bind=engine)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-
+"""
 @app.get("/")
 def read_root():
     return {"message": "Hello "}
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8000)
+
